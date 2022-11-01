bdfe2cfb5c319c3a24ed91dfee9a8d3f553ce499
Konstantsin <kostyannn1996@gmail.com>
2022-11-01 11:00:04 +0100

    removed unnecessary task

--- lesson_1/task_1/script.js
+++
@@ -1,9 +0,0 @@
-function operationsWithNumbers(firstNumber, secondNumber) {
-    if(isNaN(firstNumber) || isNaN(secondNumber)) {
-        console.log("Incorrect input!");
-    } else {
-        console.log(`First number: ${firstNumber}. Second number: ${secondNumber}. Sum: ${firstNumber + secondNumber}. Product: ${firstNumber * secondNumber}. Power: ${Math.pow(firstNumber,secondNumber)}.`);
-    }
-}
-
-operationsWithNumbers(parseInt(prompt('Enter the number')), parseInt(prompt('Enter the number')));
