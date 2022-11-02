6f16b97deadc1bae54dedb08e235b5ad25a8234b
Konstantsin <kostyannn1996@gmail.com>
2022-11-02 12:35:51 +0100

    task_1: fix bugs with single quotes

--- lesson_1/task_1/script.js
+++ lesson_1/task_1/script.js
@@ -2,14 +2,14 @@ function operationsWithNumbers(firstNumber, secondNumber) {
     let newFirstNumber;
     let newSecondNumber;
     if(firstNumber.length === 0 || secondNumber.length === 0) {
-        console.log("Incorrect input!");
+        console.log('Incorrect input!');
         return
     } else {
         newFirstNumber = Number(firstNumber);
         newSecondNumber = Number(secondNumber);
     }
     if(isNaN(newFirstNumber) || isNaN(newSecondNumber)) {
-        console.log("Incorrect input!");
+        console.log('Incorrect input!');
     } else {
         console.log(`First number: ${newFirstNumber}. Second number: ${newSecondNumber}. Sum: ${newFirstNumber + newSecondNumber}. Product: ${newFirstNumber * newSecondNumber}. Power: ${+Math.pow(newFirstNumber,newSecondNumber)}.`);
     }
