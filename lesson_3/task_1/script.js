5d29c47a6cbe6682ff0eb089fd15573f288afe31
Konstantsin <kostyannn1996@gmail.com>
2022-11-10 13:07:04 +0100

    init task_1: added data type check

--- lesson_3/task_1/script.js
+++ lesson_3/task_1/script.js
@@ -3,31 +3,31 @@ const obj = {
     b: 17
   };
   
+// const obj = [15,17]
 let arr = [11,13,15,16,17];
 
 Object.defineProperty(Array.prototype, 'customFilter', {
-    value: function(callback, context) { 
-    returnedArray = [];
-    for (let i = 0; i < this.length; i++) {
-        if (callback.apply(context, [
-            this[i], 
-            i, 
-            this
-        ])) {
-            returnedArray.push(this[i]);
+    value: function(callback, context) {
+    if(typeof callback === 'function') {
+        if(typeof context === 'undefined' || typeof context === 'object') {
+            returnedArray = [];
+            for (let i = 0; i < this.length; i++) {
+                if (callback.apply(context, [this[i], i, this])) {
+                    returnedArray.push(this[i]);
+                }
+            }
+            return returnedArray;
+        } else {
+            throw new Error();    
         }
+    } else {
+        throw new Error()
     }
-    return returnedArray;    
     }
 });
 
 
-// const filteredArr = arr.customFilter((function(item) { return item > 2 }), obj) // [3]
-const filter = arr.customFilter(function(i) {
-    return i >= this.a && i <= this.b
-  }
-  , obj)
-
-
-
-
+const filteredArr  = arr.customFilter((function(item) { return item > 15 })) // 16,17
+// const filteredArr  = arr.customFilter(function(i) {
+//     return i >= this.a && i <= this.b
+//   }, obj)
