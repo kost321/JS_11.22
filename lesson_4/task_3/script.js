e468fe6eead314a8898b671f7c924151d6749f90
Konstantsin <kostyannn1996@gmail.com>
2022-11-14 14:17:19 +0100

    task_1: add new solution task_3: add solution

--- lesson_4/task_3/script.js
+++ lesson_4/task_3/script.js
@@ -1,72 +1,63 @@
-(async () => {
-  class RickAndMorty {
+class RickAndMorty {
 
-    apiCharacters = 'https://rickandmortyapi.com/api/character';
-    apiEpisodes = 'https://rickandmortyapi.com/api/episode';
-    
-    getCharacter(idCharacter) {
-      let result;
-      if(idCharacter === undefined || typeof idCharacter !== 'number' || isNaN(idCharacter)) {
-        throw new Error();
-      }
-
-      return fetch(this.apiCharacters)
-      .then((value) => value.json())
-      .then((res) => {
-        let filterCharacter = res.results.filter((item) => {
-          if(item.id !== idCharacter) {
-            return null;
-          } else {
-            return item.id === idCharacter;
-          }
-        });
-
-        if(filterCharacter.length < 1) {
-          result = null;
-        } else {
-          result = filterCharacter[0];
-        }
-        return result;
-      })
-      .catch(err => console.log(err));
-    }
+  apiEpisodes = 'https://rickandmortyapi.com/api/episode';
+  apiCharacters = 'https://rickandmortyapi.com/api/character';
   
-    async getEpisode(idEpisode) {
+  getCharacter(idCharacter) {
+    return fetch(this.apiCharacters)
+    .then((value) => value.json())
+    .then((res) =>  {
       let result;
-      let character;
-      
-      if(idEpisode === undefined || typeof idEpisode !== 'number' || isNaN(idEpisode)) {
-        throw new Error();
-      }
-      
-      try{
-        const response = await fetch(this.apiEpisodes);
-        if(response.ok) {
-          character = await response.json();    
-        }
-      } catch (err) {
-        console.log(err);
-      }
-  
-      let episodesArr = character.results;
-      let filterEpisode = episodesArr.filter((item) => {
-        if(item.id !== idEpisode) {
-          return null;
+      let filterCharacter = res.results.filter((item) => {
+        if(item.id !== idCharacter) {
+          return null
         } else {
-          return item.id === idEpisode;
+          return item.id === idCharacter;
         }
       });
-  
-      if(filterEpisode.length < 1) {
+      if(filterCharacter.length < 1) {
         result = null;
       } else {
-        result = filterEpisode[0];
+        result = filterCharacter[0];
       }
-      return result;
-    }
+      console.log(result)
+      return result
+    })
   }
-  
-  let rickAndMorty =  new RickAndMorty();
-  await rickAndMorty.getCharacter(2);
-  await rickAndMorty.getEpisode(20);
-})();
+
+  async getEpisode(idEpisode) {
+    if(idEpisode === undefined || typeof idEpisode !== 'number') {
+      throw new Error();
+    }
+    let result;
+    let character;
+    try{
+      const response = await fetch(this.apiEpisodes);
+      if(response.ok) {
+        character = await response.json();    
+      }
+    } catch (err) {
+      console.log(err);
+    }
+
+    let episodesArr = character.results;
+    let filterEpisode = episodesArr.filter((item) => {
+      if(item.id !== idEpisode) {
+        return null
+      } else {
+        return item.id === idEpisode;
+      }
+    });
+
+    if(filterEpisode.length < 1) {
+      result = null;
+    } else {
+      result = filterEpisode[0];
+    }
+    return result;
+  }
+}
+
+let rickAndMorty = new RickAndMorty();
+console.log(rickAndMorty.getCharacter(2));
+console.log(rickAndMorty.getEpisode(2));
