f815e61a010990f4e74326529fafbc2bffcf3f4c
Konstantsin <kostyannn1996@gmail.com>
2022-11-16 15:23:27 +0100

    task_1,task_2: add current interoperability checkx

--- lesson_5/task_1/script.js
+++ lesson_5/task_1/script.js
@@ -1,12 +1,14 @@
+//---------------------------task_1---------------------------
+
 class Node {
-  constructor(val){
+  constructor(val) {
       this.val = val;
       this.next = null; 
   }
 }
 
 class Stack {
-  constructor(maxNum){
+  constructor(maxNum) {
     if(typeof maxNum === 'undefined' ) {
       this.maxNum = 10;
     } else if (!isFinite(maxNum) || isNaN(maxNum) || typeof maxNum !== 'number' || maxNum < 0) {
@@ -19,12 +21,12 @@ class Stack {
       this.length = 0;
   }
 
-  push(val){
-    let newNode = new Node(val)
-    if (this.length === 0){
+  push(val) {
+    let newNode = new Node(val);
+    if (this.length === 0) {
         this.first = newNode;
         this.last = newNode;
-    } else if(this.length === this.maxNum){
+    } else if(this.length === this.maxNum) {
       throw new Error('Limit exceeded');
     } else {
       let temp = this.first;
@@ -35,13 +37,13 @@ class Stack {
     return this.length;
   }
 
-  pop(){
+  pop() {
       if (this.length === 0) {
         throw new Error('Empty stack');
       };
       let temp = this.first;
 
-      if (this.length === 1){
+      if (this.length === 1) {
           this.last = null;
       }        
       this.first = this.first.next;        
@@ -51,7 +53,7 @@ class Stack {
       return temp.val;
   }
 
-  peek(){
+  peek() {
     if(this.length === 0) {
       return null;
     }
@@ -59,11 +61,11 @@ class Stack {
     return this.first.val;
   }
 
-  isEmpty(){
+  isEmpty() {
     return this.length === 0 ? false : true;
   }
 
-  toArray(){
+  toArray() {
     let newArr = [];
     if(this.length === 0){
       return newArr;
@@ -72,8 +74,8 @@ class Stack {
     const iterate = (obj) => {
       Object.keys(obj).forEach(key => {
       if (typeof obj[key] === 'object' && obj[key] !== null) {
-          iterate(obj[key])
-        } else if(obj[key] !== null){
+          iterate(obj[key]);
+        } else if(obj[key] !== null) {
           newArr.push(obj[key]);
         }
       })
@@ -83,19 +85,11 @@ class Stack {
   }
 
   static fromIterable(iterable) {
-    let newObj = new Stack();
-    let checkIterable = false;
-    try {
-      for(let i in iterable) {
-        break;
-      }
-      checkIterable = true; 
-    } catch(e) {
-      checkIterable = false;
+    let newObj = new Stack(iterable.length);
+    if(!(Symbol.iterator in Object(iterable))) {
+      throw new Error('Not iterable');
     }
-    if(checkIterable === false) {
-      throw new Error('Not iterable')
-    }
+   
     for(const [value, key] in iterable) {
       newObj.push(iterable[value]);    
     }
@@ -103,45 +97,27 @@ class Stack {
   }  
 }
 
-let stack = new Stack(2);
 
-// console.log(result.push(2));
-// console.log(result.push(3));
-// console.log(result.push(5));
-// console.log(result.pop());
-// console.log(result.pop());
-// console.log(result.pop());
-// let obj = '12';
-// let e = Stack.fromIterable(obj);
-// console.log(e.push(3))
-// console.log(e.peek())
-// console.log(e.isEmpty());
-// console.log(e.pop())
-// console.log(e.toArray())
-// console.log(result.toArray())
-
-
-
-//-----------------------task_2---------------------------
+//---------------------------task_2---------------------------
 
 
 class ListNode {
-  constructor(val){
+  constructor(val) {
       this.val = val;
       this.next = null; 
   }
 }
 
 class LinkedList {
-  constructor(){
+  constructor() {
       this.first = null;
       this.last = null;
       this.length = 0;
   }
 
-  append(elem){
-    let newNode = new ListNode(elem)
-    if (this.length === 0){
+  append(elem) {
+    let newNode = new ListNode(elem);
+    if (this.length === 0) {
         this.first = newNode;
         this.last = newNode;
     } else {
@@ -152,9 +128,9 @@ class LinkedList {
     return this.length;
   }
 
-  prepend(elem){
-    let newNode = new ListNode(elem)
-    if (this.length === 0){
+  prepend(elem) {
+    let newNode = new ListNode(elem); 
+    if (this.length === 0) {
         this.first = newNode;
         this.last = newNode;
     } else {
@@ -166,27 +142,27 @@ class LinkedList {
     return this.length;
   }
 
-  find(elem){
+  find(elem) {
     let currentNode = this.first;
     while(currentNode) {
-      if(currentNode.val === elem){
+      if(currentNode.val === elem) {
         return currentNode.val;
       }
 
       currentNode = currentNode.next;
     }
-    return null
+    return null;
   }
 
   toArray(){
     let newArr = [];
-    if(this.length === 0){
+    if(this.length === 0) {
       return newArr;
     }
     const iterate = (obj) => {
       Object.keys(obj).forEach(key => {
       if (typeof obj[key] === 'object' && obj[key] !== null) {
-          iterate(obj[key])
+          iterate(obj[key]);
         } else if(obj[key] !== null){
           newArr.push(obj[key]);
         }
@@ -197,19 +173,9 @@ class LinkedList {
   }
 
   static fromIterable(iterable) {
-    debugger
     let newObj = new LinkedList();
-    let checkIterable = false;
-    try {
-      for(let i in iterable) {
-        break;
-      }
-      checkIterable = true; 
-    } catch(e) {
-      checkIterable = false;
-    }
-    if(checkIterable === false) {
-      throw new Error('Not iterable')
+    if(!(Symbol.iterator in Object(iterable))) {
+      throw new Error('Not iterable');
     }
     for(const [value, key] in iterable) {
       newObj.prepend(iterable[value]);    
@@ -217,13 +183,3 @@ class LinkedList {
     return newObj;
   }  
 }
-
-let linkedList = new LinkedList();
-// linkedList.append(2);
-// linkedList.append(3);
-// linkedList.prepend(5);
-// linkedList.prepend(7);
-// obj = '23'
-// LinkedList.fromIterable(obj)
-// console.log(linkedList.find(5));
-// console.log(linkedList.toArray());
