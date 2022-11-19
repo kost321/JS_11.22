b7933cfc4d6db545e6d6a93627411132b1fd8535
Konstantsin <kostyannn1996@gmail.com>
2022-11-19 12:49:32 +0100

    homework: add style and markup

---
+++ lesson_6/calculator/script.js
@@ -0,0 +1,42 @@
+class Calculator {
+    constructor(previousOperand, currentOperand) {
+      this.previousOperand = previousOperand;
+      this.currentOperand= currentOperand;
+      this.clear();
+  }
+
+      clear() {
+        this.currentOperand = '';
+        this.previousOperand = '';
+        this.operation = undefined;
+    }
+
+    delete() {
+    }
+
+    appendNumber(number) {
+    }
+
+    chooseOperation(operation) {
+    }
+
+    compute() {
+    }
+
+    updateDisplay() {
+    }
+}
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+
+
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
